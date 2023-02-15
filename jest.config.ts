69ada558296cbeb9c6ec30e312ca758bcf16a9ac
Gabriel Ignat <gabriel.ignat@hotmail.com>
2023-02-15 13:13:29 +0000

    fix: default settings

--- jest.config.ts
+++ jest.config.ts
@@ -1,35 +1,32 @@
 import { JestConfigWithTsJest } from 'ts-jest';
 
-const moduleNameMapper = {
-  '@ukef/config/(.*)': '<rootDir>/../src/config/$1',
-  '@ukef/helpers/(.*)': '<rootDir>/../src/helpers/$1',
-  '@ukef/module/(.*)': '<rootDir>/../src/modules/$1',
-  '@ukef/(.*)': '<rootDir>/../src/$1',
+const defaultSettings = {
+  rootDir: 'test',
+  extensionsToTreatAsEsm: ['.ts'],
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  moduleNameMapper: {
+    '@ukef/config/(.*)': '<rootDir>/../src/config/$1',
+    '@ukef/helpers/(.*)': '<rootDir>/../src/helpers/$1',
+    '@ukef/module/(.*)': '<rootDir>/../src/modules/$1',
+    '@ukef/(.*)': '<rootDir>/../src/$1',
+  },
 };
 
 const config: JestConfigWithTsJest = {
   projects: [
     {
       displayName: 'Unit',
-      rootDir: 'test',
       testMatch: ['**/*.spec.ts'],
-      extensionsToTreatAsEsm: ['.ts'],
-      preset: 'ts-jest/presets/default-esm',
-      testEnvironment: 'node',
       transform: { '^.+\\.(ts|tsx)?$': ['ts-jest', { useESM: true }] },
-      moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
-      moduleNameMapper,
+      ...defaultSettings,
     },
     {
       displayName: 'API',
-      rootDir: 'test',
       testMatch: ['**/*.api-test.ts'],
-      extensionsToTreatAsEsm: ['.ts'],
-      preset: 'ts-jest/presets/default-esm',
-      testEnvironment: 'node',
       transform: { '^.+\\.(ts|tsx)?$': ['ts-jest', { useESM: true }] },
-      moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
-      moduleNameMapper,
+      ...defaultSettings,
     },
   ],
 };
