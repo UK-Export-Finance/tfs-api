df1c8f565764763b6620834380633257bcbe164e
Luke Marrai <Luke.Marrai@softwire.com>
2023-03-10 14:28:04 +0000

    refactor(APIM-78): extract ACBS HTTP error handling

--- src/modules/acbs/acbs-party-external-rating.service.ts
+++ src/modules/acbs/acbs-party-external-rating.service.ts
@@ -2,12 +2,10 @@ import { HttpService } from '@nestjs/axios';
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import AcbsConfig from '@ukef/config/acbs.config';
-import { AxiosError } from 'axios';
-import { catchError, lastValueFrom } from 'rxjs';
+import { lastValueFrom } from 'rxjs';
 
 import { AcbsPartyExternalRatingsResponseDto } from './dto/acbs-party-external-ratings-response.dto';
-import { AcbsException } from './exception/acbs.exception';
-import { AcbsResourceNotFoundException } from './exception/acbs-resource-not-found.exception';
+import { wrapAcbsHttpError } from './wrap-acbs-http-error';
 
 @Injectable()
 export class AcbsPartyExternalRatingService {
@@ -27,11 +25,9 @@ export class AcbsPartyExternalRatingService {
           },
         })
         .pipe(
-          catchError((error: Error) => {
-            if (error instanceof AxiosError && error.response && typeof error.response.data === 'string' && error.response.data.includes('Party not found')) {
-              throw new AcbsResourceNotFoundException(`Party with identifier ${partyIdentifier} was not found by ACBS.`, error);
-            }
-            throw new AcbsException(`Failed to get the external ratings for the party with id ${partyIdentifier}.`, error);
+          wrapAcbsHttpError({
+            resourceIdentifier: partyIdentifier,
+            messageForUnknownException: `Failed to get the external ratings for the party with id ${partyIdentifier}.`,
           }),
         ),
     );
