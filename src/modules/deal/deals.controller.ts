922ee39f30db05c448145b0b0f22d694883495f1
Philippa Hack <hackphilippa@gmail.com>
2023-03-14 17:12:50 +0000

    refactor(APIM-74): remove comment

--- src/modules/deal/deals.controller.ts
+++ src/modules/deal/deals.controller.ts
@@ -20,11 +20,7 @@ export class DealsController {
     description: 'An internal server error has occurred',
   })
   async createDeal(@Body() createDealDto: CreateDealDto): Promise<CreateDealResponse> {
-    const { headers } = await this.dealsService.createDeal(createDealDto);
-    // The createDeal call will return the location as a header, which we can then truncate the get the deal identifier
-    // The header value will look something like '/Portfolio/E1/Deal/0030596647', where the dealIdentifier is '0030596647'
-    // const dealIdentifier = headers['location']
+    await this.dealsService.createDeal(createDealDto);
     return { dealIdentifier: createDealDto.dealIdentifier };
-    // Comment on the PR: is it okay to use the value in the request body as opposed to what is returned in the location header
   }
 }
