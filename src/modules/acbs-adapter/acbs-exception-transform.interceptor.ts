f11555378a8dc80a74c5ca765713035935f266c5
Luke Marrai <Luke.Marrai@softwire.com>
2023-03-23 15:46:48 +0000

    refactor(APIM-73): remove nested ternary statement

--- src/modules/acbs-adapter/acbs-exception-transform.interceptor.ts
+++ src/modules/acbs-adapter/acbs-exception-transform.interceptor.ts
@@ -7,18 +7,20 @@ import { AcbsBadRequestException } from '../acbs/exception/acbs-bad-request.exce
 @Injectable()
 export class AcbsExceptionTransformInterceptor implements NestInterceptor {
   intercept(_context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next
-      .handle()
-      .pipe(
-        catchError((err) =>
-          throwError(() =>
-            err instanceof AcbsResourceNotFoundException
-              ? new NotFoundException('Not found', { cause: err })
-              : err instanceof AcbsBadRequestException
-              ? new BadRequestException('Bad request', { cause: err, description: err.errorBody })
-              : err,
-          ),
-        ),
-      );
+    return next.handle().pipe(
+      catchError((err) =>
+        throwError(() => {
+          if (err instanceof AcbsResourceNotFoundException) {
+            return new NotFoundException('Not found', { cause: err });
+          }
+
+          if (err instanceof AcbsBadRequestException) {
+            return new BadRequestException('Bad request', { cause: err, description: err.errorBody });
+          }
+
+          return err;
+        }),
+      ),
+    );
   }
 }
