0cd4db9cbfe01ebe4542e555c24f2d34305739b5
Luke Marrai <Luke.Marrai@softwire.com>
2023-03-27 16:39:08 +0100

    refactor: commonise acbs http service

--- src/modules/acbs/acbs-deal-guarantee.service.ts
+++ src/modules/acbs/acbs-deal-guarantee.service.ts
@@ -3,34 +3,33 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import AcbsConfig from '@ukef/config/acbs.config';
 import { PROPERTIES } from '@ukef/constants';
-import { AxiosResponse } from 'axios';
-import { lastValueFrom } from 'rxjs';
 
+import { AcbsHttpService } from './acbs-http.service';
 import { AcbsCreateDealGuaranteeDto } from './dto/acbs-create-deal-guarantee.dto';
-import { wrapAcbsHttpPostError } from './wrap-acbs-http-error';
+import { createWrapAcbsHttpPostErrorCallback } from './wrap-acbs-http-error-callback';
 
 @Injectable()
 export class AcbsDealGuaranteeService {
+  private readonly acbsHttpService: AcbsHttpService;
+
   constructor(
     @Inject(AcbsConfig.KEY)
-    private readonly config: Pick<ConfigType<typeof AcbsConfig>, 'baseUrl'>,
-    private readonly httpService: HttpService,
-  ) {}
+    config: Pick<ConfigType<typeof AcbsConfig>, 'baseUrl'>,
+    httpService: HttpService,
+  ) {
+    this.acbsHttpService = new AcbsHttpService(config, httpService);
+  }
 
   async createGuaranteeForDeal(dealIdentifier: string, newDealGuarantee: AcbsCreateDealGuaranteeDto, idToken: string): Promise<void> {
     const portfolioIdentifier = PROPERTIES.GLOBAL.portfolioIdentifier;
-    await lastValueFrom(
-      this.httpService
-        .post<never>(`/Portfolio/${portfolioIdentifier}/Deal/${dealIdentifier}/DealGuarantee`, newDealGuarantee, {
-          baseURL: this.config.baseUrl,
-          headers: { Authorization: `Bearer ${idToken}`, 'Content-Type': 'application/json' },
-        })
-        .pipe(
-          wrapAcbsHttpPostError<AxiosResponse<never, any>, any>({
-            resourceIdentifier: dealIdentifier,
-            messageForUnknownException: `Failed to create a guarantee for deal ${dealIdentifier} in ACBS.`,
-          }),
-        ),
-    );
+    await this.acbsHttpService.post<AcbsCreateDealGuaranteeDto>({
+      path: `/Portfolio/${portfolioIdentifier}/Deal/${dealIdentifier}/DealGuarantee`,
+      requestBody: newDealGuarantee,
+      idToken,
+      onError: createWrapAcbsHttpPostErrorCallback({
+        resourceIdentifier: dealIdentifier,
+        messageForUnknownException: `Failed to create a guarantee for deal ${dealIdentifier} in ACBS.`,
+      }),
+    });
   }
 }
--- src/modules/acbs/wrap-acbs-http-error-callback.ts
+++ src/modules/acbs/wrap-acbs-http-error-callback.ts
@@ -1,35 +1,25 @@
 import { AxiosError } from 'axios';
-import { catchError, ObservableInput, ObservedValueOf, OperatorFunction } from 'rxjs';
 
 import { AcbsException } from './exception/acbs.exception';
 import { AcbsBadRequestException } from './exception/acbs-bad-request.exception';
 import { AcbsResourceNotFoundException } from './exception/acbs-resource-not-found.exception';
 import { AcbsUnexpectedException } from './exception/acbs-unexpected.exception';
 
-export function wrapAcbsHttpError<T, O extends ObservableInput<any>>({
-  resourceIdentifier,
-  messageForUnknownException,
-}: {
-  resourceIdentifier: string;
-  messageForUnknownException: string;
-}): OperatorFunction<T, T | ObservedValueOf<O>> {
-  return catchError((error: Error) => {
+type AcbsHttpErrorCallback = (error: Error) => never;
+
+export const createWrapAcbsHttpErrorCallback =
+  ({ resourceIdentifier, messageForUnknownException }: { resourceIdentifier: string; messageForUnknownException: string }): AcbsHttpErrorCallback =>
+  (error: Error) => {
     if (error instanceof AxiosError && error.response && typeof error.response.data === 'string' && error.response.data.includes('Party not found')) {
       throw new AcbsResourceNotFoundException(`Party with identifier ${resourceIdentifier} was not found by ACBS.`, error);
     }
 
     throw new AcbsException(messageForUnknownException, error);
-  });
-}
+  };
 
-export function wrapAcbsHttpPostError<T, O extends ObservableInput<any>>({
-  resourceIdentifier,
-  messageForUnknownException,
-}: {
-  resourceIdentifier: string;
-  messageForUnknownException: string;
-}): OperatorFunction<T, T | ObservedValueOf<O>> {
-  return catchError((error: Error) => {
+export const createWrapAcbsHttpPostErrorCallback =
+  ({ resourceIdentifier, messageForUnknownException }: { resourceIdentifier: string; messageForUnknownException: string }): AcbsHttpErrorCallback =>
+  (error: Error) => {
     if (!(error instanceof AxiosError) || !error.response || error.response.status !== 400) {
       throw new AcbsUnexpectedException(messageForUnknownException, error);
     }
@@ -49,5 +39,4 @@ export function wrapAcbsHttpPostError<T, O extends ObservableInput<any>>({
       error,
       typeof error.response.data === 'string' ? error.response.data : JSON.stringify(error.response.data),
     );
-  });
-}
+  };
