8b9a80d497cd5246088e779d05aa5c72ff6d1d43
oscar-richardson-softwire <oscar.richardson@softwire.com>
2023-04-18 11:46:07 +0100

    feat(APIM-118): add api tests (failing)

---
+++ test/facility-covenant/get-facility-covenants-by-facility-identifier.api-test.ts
@@ -0,0 +1,135 @@
+import { PROPERTIES } from '@ukef/constants';
+import { DateStringTransformations } from '@ukef/modules/date/date-string.transformations';
+import { withAcbsAuthenticationApiTests } from '@ukef-test/common-tests/acbs-authentication-api-tests';
+import { IncorrectAuthArg, withClientAuthenticationTests } from '@ukef-test/common-tests/client-authentication-api-tests';
+import { Api } from '@ukef-test/support/api';
+import { ENVIRONMENT_VARIABLES, TIME_EXCEEDING_ACBS_TIMEOUT } from '@ukef-test/support/environment-variables';
+import { GetFacilityCovenantGenerator } from '@ukef-test/support/generator/get-facility-covenant-generator';
+import { RandomValueGenerator } from '@ukef-test/support/generator/random-value-generator';
+import nock from 'nock';
+
+describe('GET /facilities/{facilityIdentifier}/covenant', () => {
+  const valueGenerator = new RandomValueGenerator();
+  const dateStringTransformations = new DateStringTransformations();
+  const portfolioIdentifier = PROPERTIES.GLOBAL.portfolioIdentifier;
+  const facilityIdentifier = valueGenerator.ukefId();
+  const getFacilityCovenantsUrl = `/api/v1/facilities/${facilityIdentifier}/covenant`;
+
+  const { facilityCovenantsInAcbs, facilityCovenantsFromApi } = new GetFacilityCovenantGenerator(valueGenerator, dateStringTransformations).generate({
+    numberToGenerate: 2,
+    facilityIdentifier,
+    portfolioIdentifier,
+  });
+
+  let api: Api;
+
+  beforeAll(async () => {
+    api = await Api.create();
+  });
+
+  afterAll(async () => {
+    await api.destroy();
+  });
+
+  afterEach(() => {
+    nock.abortPendingRequests();
+    nock.cleanAll();
+  });
+
+  const { idToken, givenAuthenticationWithTheIdpSucceeds } = withAcbsAuthenticationApiTests({
+    givenRequestWouldOtherwiseSucceed: () => requestToGetFacilityCovenants().reply(200, facilityCovenantsInAcbs),
+    makeRequest: () => api.get(getFacilityCovenantsUrl),
+  });
+
+  withClientAuthenticationTests({
+    givenTheRequestWouldOtherwiseSucceed: () => {
+      givenAuthenticationWithTheIdpSucceeds();
+      requestToGetFacilityCovenants().reply(200, facilityCovenantsInAcbs);
+    },
+    makeRequestWithoutAuth: (incorrectAuth?: IncorrectAuthArg) =>
+      api.getWithoutAuth(getFacilityCovenantsUrl, incorrectAuth?.headerName, incorrectAuth?.headerValue),
+  });
+
+  it('returns a 200 response with the facility covenants if they are returned by ACBS', async () => {
+    givenAuthenticationWithTheIdpSucceeds();
+    requestToGetFacilityCovenants().reply(200, facilityCovenantsInAcbs);
+
+    const { status, body } = await api.get(getFacilityCovenantsUrl);
+
+    expect(status).toBe(200);
+    expect(body).toStrictEqual(JSON.parse(JSON.stringify(facilityCovenantsFromApi)));
+  });
+
+  it('returns a 404 response with an empty array if ACBS returns a 200 response with an empty array', async () => {
+    givenAuthenticationWithTheIdpSucceeds();
+    requestToGetFacilityCovenants().reply(200, []);
+
+    const { status, body } = await api.get(getFacilityCovenantsUrl);
+
+    expect(status).toBe(200);
+    expect(body).toStrictEqual([]);
+  });
+
+  it('returns a 200 response with the facility covenants if they are returned by ACBS and an EffectiveDate field is null', async () => {
+    const facilityCovenantsInAcbsWithANullEffectiveDate = [{ ...facilityCovenantsInAcbs[0], EffectiveDate: null }, facilityCovenantsInAcbs[1]];
+    const facilityCovenantsFromApiWithANullEffectiveDate = [
+      { ...facilityCovenantsFromApi[0], guaranteeCommencementDate: null, effectiveDate: null },
+      facilityCovenantsFromApi[1],
+    ];
+
+    givenAuthenticationWithTheIdpSucceeds();
+    requestToGetFacilityCovenants().reply(200, facilityCovenantsInAcbsWithANullEffectiveDate);
+
+    const { status, body } = await api.get(getFacilityCovenantsUrl);
+
+    expect(status).toBe(200);
+    expect(body).toStrictEqual(JSON.parse(JSON.stringify(facilityCovenantsFromApiWithANullEffectiveDate)));
+  });
+
+  it('returns a 200 response with the facility covenants if they are returned by ACBS and an ExpirationDate field is null', async () => {
+    const facilityCovenantsInAcbsWithANullExpirationDate = [{ ...facilityCovenantsInAcbs[0], ExpirationDate: null }, facilityCovenantsInAcbs[1]];
+    const facilityCovenantsFromApiWithANullExpirationDate = [{ ...facilityCovenantsFromApi[0], guaranteeExpiryDate: null }, facilityCovenantsFromApi[1]];
+
+    givenAuthenticationWithTheIdpSucceeds();
+    requestToGetFacilityCovenants().reply(200, facilityCovenantsInAcbsWithANullExpirationDate);
+
+    const { status, body } = await api.get(getFacilityCovenantsUrl);
+
+    expect(status).toBe(200);
+    expect(body).toStrictEqual(JSON.parse(JSON.stringify(facilityCovenantsFromApiWithANullExpirationDate)));
+  });
+
+  it('returns a 500 response if getting the facility covenants from ACBS returns a status code that is NOT 200', async () => {
+    givenAuthenticationWithTheIdpSucceeds();
+    requestToGetFacilityCovenants().reply(401);
+
+    const { status, body } = await api.get(getFacilityCovenantsUrl);
+
+    expect(status).toBe(500);
+    expect(body).toStrictEqual({
+      statusCode: 500,
+      message: 'Internal server error',
+    });
+  });
+
+  it('returns a 500 response if getting the facility covenants from ACBS times out', async () => {
+    givenAuthenticationWithTheIdpSucceeds();
+    requestToGetFacilityCovenants().delay(TIME_EXCEEDING_ACBS_TIMEOUT).reply(200, facilityCovenantsInAcbs);
+
+    const { status, body } = await api.get(getFacilityCovenantsUrl);
+
+    expect(status).toBe(500);
+    expect(body).toStrictEqual({
+      statusCode: 500,
+      message: 'Internal server error',
+    });
+  });
+
+  // TO-DO: add tests that check we respond with 400 if the facilityId is of the wrong length/format once injectable tests for this
+  // have been written.
+
+  const requestToGetFacilityCovenants = () =>
+    nock(ENVIRONMENT_VARIABLES.ACBS_BASE_URL)
+      .get(`/Portfolio/${portfolioIdentifier}/Facility/${facilityIdentifier}/Covenant`)
+      .matchHeader('authorization', `Bearer ${idToken}`);
+});
