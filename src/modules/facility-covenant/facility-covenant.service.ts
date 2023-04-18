33b57490dde30faeae81e7801e864e83228b270a
oscar-richardson-softwire <oscar.richardson@softwire.com>
2023-04-18 14:25:55 +0100

    feat(APIM-118): add facility covenant service and unit tests (passing)

---
+++ src/modules/facility-covenant/facility-covenant.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { PROPERTIES } from '@ukef/constants';
+import { AcbsFacilityCovenantService } from '@ukef/modules/acbs/acbs-facility-covenant.service';
+import { AcbsAuthenticationService } from '@ukef/modules/acbs-authentication/acbs-authentication.service';
+import { DateStringTransformations } from '@ukef/modules/date/date-string.transformations';
+
+import { GetFacilityCovenantsResponseDto } from './dto/get-facility-covenants-response.dto';
+
+@Injectable()
+export class FacilityCovenantService {
+  constructor(
+    private readonly acbsAuthenticationService: AcbsAuthenticationService,
+    private readonly acbsFacilityCovenantService: AcbsFacilityCovenantService,
+    private readonly dateStringTransformations: DateStringTransformations,
+  ) {}
+
+  async getCovenantsForFacility(facilityIdentifier: string): Promise<GetFacilityCovenantsResponseDto[]> {
+    const portfolioIdentifier = PROPERTIES.GLOBAL.portfolioIdentifier;
+    const idToken = await this.acbsAuthenticationService.getIdToken();
+    const covenantsInAcbs = await this.acbsFacilityCovenantService.getCovenantsForFacility(portfolioIdentifier, facilityIdentifier, idToken);
+    return covenantsInAcbs.map((covenant) => {
+      const effectiveDateOnly = this.dateStringTransformations.removeTimeIfExists(covenant.EffectiveDate);
+      const expirationDateOnly = this.dateStringTransformations.removeTimeIfExists(covenant.ExpirationDate);
+      return {
+        covenantIdentifier: covenant.CovenantIdentifier,
+        covenantType: covenant.CovenantType.CovenantTypeCode,
+        facilityIdentifier: facilityIdentifier,
+        portfolioIdentifier: portfolioIdentifier,
+        maximumLiability: covenant.TargetAmount,
+        currency: covenant.PledgeType.PledgeTypeCode,
+        guaranteeCommencementDate: effectiveDateOnly,
+        effectiveDate: effectiveDateOnly,
+        guaranteeExpiryDate: expirationDateOnly,
+      };
+    });
+  }
+}
