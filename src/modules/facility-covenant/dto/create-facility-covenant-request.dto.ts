fe7f067234950d13e3772e4e760db5abc568829e
oscar-richardson-softwire <oscar.richardson@softwire.com>
2023-04-20 11:35:23 +0100

    feat(APIM-106): add DTOs and generator

---
+++ src/modules/facility-covenant/dto/create-facility-covenant-request.dto.ts
@@ -0,0 +1,59 @@
+import { ENUMS } from '@ukef/constants';
+import { ValidatedDateOnlyApiProperty } from '@ukef/decorators/validated-date-only-api-property.decorator';
+import { ValidatedNumberApiProperty } from '@ukef/decorators/validated-number-api-property.decorator';
+import { ValidatedStringApiProperty } from '@ukef/decorators/validated-string-api-property.decorator';
+import { DateOnlyString } from '@ukef/helpers';
+
+export type CreateFacilityCovenantRequest = CreateFacilityCovenantRequestItem[];
+
+export class CreateFacilityCovenantRequestItem {
+  @ValidatedStringApiProperty({
+    description: 'The identifier of the covenant in ACBS. When creating a covenant an identifier from the number generator should be used.',
+    example: '0000000001',
+    length: 10,
+  })
+  readonly facilityIdentifier: string;
+
+  @ValidatedStringApiProperty({
+    description: 'The identifier of the facility to create the covenant for in ACBS.',
+    example: '0000000001',
+    length: 10,
+  })
+  readonly covenantIdentifier: string; // TO-DO: in Mulesoft it looks like this can be undefined/null/empty string, in which
+  // case it gets transformed to null and ACBS will generate an ID for the covenant without checking if this ID has already
+  // been used. Presumably this is behaviour we want to avoid?
+
+  @ValidatedStringApiProperty({
+    description:
+      'The covenant type code: 43 for a UK Contract Value covenant, 46 for a Chargable Amount covenant, and 47 for a chargable Amount covenant not in GBP.',
+    example: '43',
+    enum: ENUMS.COVENANT_TYPE_CODES,
+  })
+  readonly covenantType: string;
+
+  @ValidatedNumberApiProperty({
+    description: 'The amount used to determine if the covenant is in compliance or not. It is called target amount in ACBS.',
+    minimum: 0,
+  })
+  readonly maximumLiability: number;
+
+  @ValidatedStringApiProperty({
+    description: 'The covenant currency type code. The maximum number of characters allowed is 3. It is called pledge type code in ACBS.',
+    example: 'N',
+    minLength: 0,
+    maxLength: 3,
+  })
+  readonly currency: string;
+
+  @ValidatedDateOnlyApiProperty({
+    description: 'The expiration date of the covenant. It is called expiration date in ACBS.',
+    example: '2023-04-19',
+  })
+  guaranteeExpiryDate: DateOnlyString;
+
+  @ValidatedDateOnlyApiProperty({
+    description: 'The effective date of the covenant.',
+    example: '2023-04-19',
+  })
+  effectiveDate: DateOnlyString;
+}
---
+++ test/support/generator/create-facility-covenant-generator.ts
@@ -0,0 +1,121 @@
+import { ENUMS, PROPERTIES } from '@ukef/constants';
+import { UkefId } from '@ukef/helpers';
+import { AcbsCreateFacilityCovenantRequest } from '@ukef/modules/acbs/dto/acbs-create-facility-covenant-request.dto';
+import { CurrentDateProvider } from '@ukef/modules/date/current-date.provider';
+import { DateStringTransformations } from '@ukef/modules/date/date-string.transformations';
+import { CreateFacilityCovenantRequest, CreateFacilityCovenantRequestItem } from '@ukef/modules/facility-covenant/dto/create-facility-covenant-request.dto';
+
+import { AbstractGenerator } from './abstract-generator';
+import { RandomValueGenerator } from './random-value-generator';
+
+export class CreateFacilityCovenantGenerator extends AbstractGenerator<CreateFacilityCovenantRequestItem, GenerateResult, GenerateOptions> {
+  constructor(
+    protected readonly valueGenerator: RandomValueGenerator,
+    protected readonly currentDateProvider: CurrentDateProvider,
+    protected readonly dateStringTransformations: DateStringTransformations,
+  ) {
+    super(valueGenerator);
+  }
+
+  protected generateValues(): CreateFacilityCovenantRequestItem {
+    return {
+      facilityIdentifier: this.valueGenerator.ukefId(),
+      covenantIdentifier: this.valueGenerator.ukefId(), // TO-DO: is this right to use ukefId()?
+      covenantType: ['43', '46', '47'][this.valueGenerator.integer({ min: 0, max: 2 })],
+      maximumLiability: this.valueGenerator.nonnegativeFloat({ fixed: 2 }), // TO-DO: is this right to use fixed: 2?
+      currency: this.valueGenerator.string({ minLength: 0, maxLength: 3 }),
+      guaranteeExpiryDate: this.valueGenerator.dateOnlyString(),
+      effectiveDate: this.valueGenerator.dateOnlyString(),
+    };
+  }
+
+  protected transformRawValuesToGeneratedValues(
+    values: CreateFacilityCovenantRequest,
+    { covenantIdentifier, facilityIdentifier, facilityTypeCode, limitKeyValue }: GenerateOptions,
+  ): GenerateResult {
+    const firstFacilityCovenant = values[0];
+
+    let covenantName;
+
+    if (
+      firstFacilityCovenant.covenantType === ENUMS.COVENANT_TYPE_CODES.CHARGABLE_AMOUNT ||
+      firstFacilityCovenant.covenantType === ENUMS.COVENANT_TYPE_CODES.CHARGABLE_AMOUNT_NOT_GBP
+    ) {
+      covenantName = 'CHARGABLE AMOUNT';
+    } else if (facilityTypeCode === '250') {
+      covenantName = 'AMOUNT OF SUPPORTED BOND';
+    } else if (
+      facilityTypeCode === '260' ||
+      (firstFacilityCovenant.covenantType === ENUMS.COVENANT_TYPE_CODES.UK_CONTRACT_VALUE && facilityTypeCode === '280')
+    ) {
+      covenantName = 'AMOUNT OF SUPPORTED FACILITY';
+    } else {
+      covenantName = facilityTypeCode;
+    }
+
+    const effectiveDateString = this.dateStringTransformations.addTimeToDateOnlyString(firstFacilityCovenant.effectiveDate);
+    const guaranteeExpiryDateString = this.dateStringTransformations.addTimeToDateOnlyString(firstFacilityCovenant.guaranteeExpiryDate);
+
+    const acbsRequestBodyToCreateFacilityCovenant: AcbsCreateFacilityCovenantRequest = {
+      AccountOwnerIdentifier: PROPERTIES.COVENANT.DEFAULTS.accountOwnerIdentifier,
+      ComplianceEvaluationMode: {
+        CovenantEvaluationModeCode: PROPERTIES.COVENANT.DEFAULTS.complianceEvaluationMode.covenantEvaluationModeCode,
+      },
+      ComplianceStatusDate: effectiveDateString,
+      CovenantIdentifier: firstFacilityCovenant.covenantIdentifier,
+      CovenantName: covenantName,
+      DateCycleEvaluationMode: {
+        CovenantEvaluationModeCode: PROPERTIES.COVENANT.DEFAULTS.dateCycleEvaluationMode.covenantEvaluationModeCode,
+      },
+      EffectiveDate: effectiveDateString,
+      ExpirationDate: guaranteeExpiryDateString,
+      LenderType: {
+        LenderTypeCode: PROPERTIES.COVENANT.DEFAULTS.lenderType.covenantLenderTypeCode,
+      },
+      LimitKeyValue: limitKeyValue,
+      LimitType: {
+        LimitTypeCode: PROPERTIES.COVENANT.DEFAULTS.limitType.covenantLimitTypeCode,
+      },
+      SectionIdentifier: PROPERTIES.COVENANT.DEFAULTS.sectionIdentifier,
+      TargetAmount: firstFacilityCovenant.maximumLiability,
+      PledgeType: {
+        PledgeTypeCode: firstFacilityCovenant.currency,
+      },
+      CovenantType: {
+        CovenantTypeCode: firstFacilityCovenant.covenantType,
+      },
+      ComplianceRule: {
+        ComplianceRuleCode: PROPERTIES.COVENANT.DEFAULTS.complianceRule.covenantComplianceRuleCode,
+      },
+      InComplianceIndicator: PROPERTIES.COVENANT.DEFAULTS.inComplianceIndicator,
+      WaivedIndicator: PROPERTIES.COVENANT.DEFAULTS.waivedIndicator,
+      NextReviewDate: effectiveDateString,
+    };
+
+    const requestBodyToCreateFacilityCovenant = values.map((v, index) => ({
+      facilityIdentifier,
+      covenantIdentifier: index === 0 ? covenantIdentifier : v.covenantIdentifier,
+      covenantType: v.covenantType,
+      maximumLiability: v.maximumLiability,
+      currency: v.currency,
+      guaranteeExpiryDate: v.guaranteeExpiryDate,
+      effectiveDate: v.effectiveDate,
+    }));
+
+    return {
+      acbsRequestBodyToCreateFacilityCovenant,
+      requestBodyToCreateFacilityCovenant,
+    };
+  }
+}
+interface GenerateOptions {
+  covenantIdentifier: UkefId;
+  facilityIdentifier: UkefId;
+  facilityTypeCode: string;
+  limitKeyValue: string;
+}
+
+interface GenerateResult {
+  acbsRequestBodyToCreateFacilityCovenant: AcbsCreateFacilityCovenantRequest;
+  requestBodyToCreateFacilityCovenant: CreateFacilityCovenantRequest;
+}
