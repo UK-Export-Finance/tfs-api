43f1a1c3f81f51b3955d7ab9661b4861617684ad
oscar-richardson-softwire <oscar.richardson@softwire.com>
2023-05-12 15:25:20 +0100

    refactor(APIM-256): reformat logs for consistency

--- src/helpers/logging-interceptor.helper.ts
+++ src/helpers/logging-interceptor.helper.ts
@@ -1,19 +1,19 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { Logger } from 'nestjs-pino';
+import { PinoLogger } from 'nestjs-pino';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  constructor(private readonly logger: Logger) {}
+  constructor(private readonly logger: PinoLogger) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const requestBody = context.switchToHttp().getRequest().body;
-    this.logger.debug(`Request body: ${JSON.stringify(requestBody)}`);
+    this.logger.debug({ requestBody }, 'Handling the following request from the client.');
 
     return next.handle().pipe(
       tap((responseBody) => {
-        this.logger.debug(`Response body: ${JSON.stringify(responseBody)}`);
+        this.logger.debug({ responseBody }, 'Returning the following response to the client.');
       }),
     );
   }
