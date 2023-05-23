d3a839a3a0ad921b70b23863982dff35363442f3
Luke Marrai <Luke.Marrai@softwire.com>
2023-05-23 15:51:44 +0100

    refactor(APIM-222): refactor empty array check

--- src/logging/build-key-to-redact.ts
+++ src/logging/build-key-to-redact.ts
@@ -3,7 +3,7 @@ const suffix = `"]`;
 const joinSeparator = `${suffix}${prefix}`;
 
 export const buildKeyToRedact = (parts: string[]): string => {
-  if (parts.length === 0) {
+  if (!parts.length) {
     return '';
   }
   return `${prefix}${parts.join(joinSeparator)}${suffix}`;
