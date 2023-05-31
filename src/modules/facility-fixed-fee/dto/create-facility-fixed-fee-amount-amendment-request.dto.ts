190990d09558e7e5159188fc7fba60c2fbeb859f
Audrius Vaitonis <audrius.vaitonis@ukexportfinance.gov.uk>
2023-05-31 17:11:57 +0100

    feat(APIM-113): one new description

--- src/modules/facility-fixed-fee/dto/create-facility-fixed-fee-amount-amendment-request.dto.ts
+++ src/modules/facility-fixed-fee/dto/create-facility-fixed-fee-amount-amendment-request.dto.ts
@@ -10,7 +10,7 @@ export type CreateFixedFeeAmountAmendmentRequest = CreateFixedFeeAmountAmendment
 
 export class CreateFixedFeeAmountAmendmentRequestItem {
   @ValidatedPartyIdentifierApiProperty({
-    description: '',
+    description: 'ACBS id for Exporter party record.',
   })
   readonly partyIdentifier: AcbsPartyId;
 
