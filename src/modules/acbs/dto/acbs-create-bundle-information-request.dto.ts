fb22e72d319930532a08cb2683bab3d8712a6adb
lmarrai-sw <44811960+lmarrai-sw@users.noreply.github.com>
2023-06-02 19:23:03 +0100

    refactor: remove todos for APIM-311 (#211)

--- src/modules/acbs/dto/acbs-create-bundle-information-request.dto.ts
+++ src/modules/acbs/dto/acbs-create-bundle-information-request.dto.ts
@@ -4,7 +4,6 @@ import { BundleAction } from './bundle-actions/bundle-action.type';
 
 export interface AcbsCreateBundleInformationRequestDto<BundleMessageListItem extends BundleAction = BundleAction> {
   PortfolioIdentifier: string;
-  // TODO APIM-311: Replace InitialBundleStatusCode with RequestedBundleStatus, as InitialBundleStatusCode is deprecated as of 01 dec 2024.
   InitialBundleStatusCode: number;
   InitiatingUserName: string;
   UseAPIUserIndicator: boolean;
