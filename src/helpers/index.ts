c13092d14e1b518d95c3793dca47c48ff570d6ec
AlexBramhill <47955140+AlexBramhill@users.noreply.github.com>
2023-07-10 18:39:44 +0100

    fix(APIM-531): fix swagger regex field (#308)

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,6 @@
 export * from './acbs-id.type';
 export * from './date-only-string.type';
 export * from './date-string.type';
+export * from './regex.helper';
 export * from './transform-interceptor.helper';
 export * from './ukef-id.type';
