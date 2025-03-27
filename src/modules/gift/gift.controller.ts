4b25a6e3e69bfba3db7668f0f0698ab70fb3a53e
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2025-03-27 16:54:55 +0000

    feat(GIFT-10497): remove unused import

--- src/modules/gift/gift.controller.ts
+++ src/modules/gift/gift.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Res, Version } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
   ApiInternalServerErrorResponse,
