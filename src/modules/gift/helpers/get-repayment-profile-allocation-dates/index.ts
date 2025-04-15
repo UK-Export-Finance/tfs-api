336bf3b8d530e5cde5289e8aeb8fe0ac528d9a72
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2025-04-15 14:37:58 +0100

    chore(GIFT-11460): fix typo

--- src/modules/gift/helpers/get-repayment-profile-allocation-dates/index.ts
+++ src/modules/gift/helpers/get-repayment-profile-allocation-dates/index.ts
@@ -2,7 +2,7 @@ import { GiftRepaymentProfileDto } from '../../dto';
 
 /**
  * Get all allocation dates from GIFT repayment profile allocations
- * @param {GiftRepaymentProfileDto[]} repaymentProfiles: ALl repayment profiles
+ * @param {GiftRepaymentProfileDto[]} repaymentProfiles: All repayment profiles
  * @returns {string[]} All allocation's dueDate values
  */
 export const getRepaymentProfileAllocationDates = (repaymentProfiles: GiftRepaymentProfileDto[]): string[] => {
--- src/modules/gift/helpers/get-repayment-profile-names/index.ts
+++ src/modules/gift/helpers/get-repayment-profile-names/index.ts
@@ -2,7 +2,7 @@ import { GiftRepaymentProfileDto } from '../../dto';
 
 /**
  * Get all names from GIFT repayment profiles
- * @param {GiftRepaymentProfileDto[]} repaymentProfiles: ALl repayment profiles
+ * @param {GiftRepaymentProfileDto[]} repaymentProfiles: All repayment profiles
  * @returns {string[]} All repayment profile's name values
  */
 export const getRepaymentProfileNames = (repaymentProfiles: GiftRepaymentProfileDto[]): string[] => {
