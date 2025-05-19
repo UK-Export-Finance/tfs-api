fda3392ff38b2aa5c060ea9bbf1ce13aaadbd89e
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2025-05-19 13:04:34 +0100

    feat(GIFT-10027): create a facility - approved status (#1015)

--- src/constants/gift/gift.constant.ts
+++ src/constants/gift/gift.constant.ts
@@ -3,6 +3,7 @@ import { VALIDATION } from './validation.constant';
 export const GIFT = {
   API_RESPONSE_MESSAGES: {
     FACILITY_VALIDATION_ERRORS: 'Validation errors with facility entity(s)',
+    APPROVED_STATUS_ERROR_MESSAGE: 'Error updating GIFT work package status to approved',
   },
   API_RESPONSE_TYPES: {
     ERROR: 'api-error-response',
@@ -27,6 +28,7 @@ export const GIFT = {
     },
   },
   PATH: {
+    APPROVE: '/approve',
     CURRENCY: '/currency',
     CONFIGURATION_EVENT: '/configuration-event/type',
     CREATE_FACILITY: '/work-package/create-facility',
