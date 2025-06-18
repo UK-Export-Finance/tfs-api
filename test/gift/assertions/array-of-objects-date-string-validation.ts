3a4284d9c62880c3c7633b14cce6c9d734a33d33
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2025-06-18 14:13:01 +0100

    chore(GIFT-12383): improve date field validation

---
+++ test/gift/assertions/array-of-objects-date-string-validation.ts
@@ -0,0 +1,369 @@
+import { GIFT } from '@ukef/constants';
+import { Api } from '@ukef-test/support/api';
+
+import { generatePayloadArrayOfObjects } from './generate-payload';
+import { assert400Response } from './response-assertion';
+
+/**
+ * Validation tests for an array of objects - date string field (ISO_639-1) with invalid values
+ * @param {String} fieldName: The name of a field. E.g, email
+ * @param {Object} initialPayload: The payload to use before adding a field value
+ * @param {String} parentFieldName: The name of a parent field. E.g parentObject
+ * @param {String} url: The URL the tests will call.
+ */
+export const arrayOfObjectsDateStringValidation = ({ fieldName, initialPayload, parentFieldName, url }) => {
+  let api: Api;
+
+  const min = GIFT.VALIDATION.DATE_STRING.MIN_LENGTH;
+  const max = GIFT.VALIDATION.DATE_STRING.MAX_LENGTH;
+
+  const payloadParams = { initialPayload, fieldName, parentFieldName };
+
+  beforeAll(async () => {
+    api = await Api.create();
+  });
+
+  afterAll(async () => {
+    await api.destroy();
+  });
+
+  describe(`when ${fieldName} is null`, () => {
+    let mockPayload;
+
+    beforeAll(() => {
+      // Arrange
+      mockPayload = generatePayloadArrayOfObjects({ ...payloadParams, value: null });
+    });
+
+    it('should return a 400 response', async () => {
+      // Act
+      const response = await api.post(url, mockPayload);
+
+      // Assert
+      assert400Response(response);
+    });
+
+    it('should return the correct error messages', async () => {
+      // Act
+      const { body } = await api.post(url, mockPayload);
+
+      // Assert
+      const expected = [
+        `${parentFieldName}.0.${fieldName} should not be null or undefined`,
+        `${parentFieldName}.0.${fieldName} must be a valid ISO 8601 date string`,
+        `${parentFieldName}.1.${fieldName} should not be null or undefined`,
+        `${parentFieldName}.1.${fieldName} must be a valid ISO 8601 date string`,
+      ];
+
+      expect(body.message).toStrictEqual(expected);
+    });
+  });
+
+  describe(`when ${fieldName} is undefined`, () => {
+    let mockPayload;
+
+    beforeAll(() => {
+      // Arrange
+      mockPayload = generatePayloadArrayOfObjects({ ...payloadParams, value: undefined });
+    });
+
+    it('should return a 400 response', async () => {
+      // Act
+      const response = await api.post(url, mockPayload);
+
+      // Assert
+      assert400Response(response);
+    });
+
+    it('should return the correct error messages', async () => {
+      // Act
+      const { body } = await api.post(url, mockPayload);
+
+      // Assert
+      const expected = [
+        `${parentFieldName}.0.${fieldName} should not be null or undefined`,
+        `${parentFieldName}.0.${fieldName} must be a valid ISO 8601 date string`,
+        `${parentFieldName}.1.${fieldName} should not be null or undefined`,
+        `${parentFieldName}.1.${fieldName} must be a valid ISO 8601 date string`,
+      ];
+
+      expect(body.message).toStrictEqual(expected);
+    });
+  });
+
+  describe(`when ${fieldName} is an empty array`, () => {
+    let mockPayload;
+
+    beforeAll(() => {
+      // Arrange
+      mockPayload = generatePayloadArrayOfObjects({ ...payloadParams, value: [] });
+    });
+
+    it('should return a 400 response', async () => {
+      // Act
+      const response = await api.post(url, mockPayload);
+
+      // Assert
+      assert400Response(response);
+    });
+
+    it('should return the correct error messages', async () => {
+      // Act
+      const { body } = await api.post(url, mockPayload);
+
+      // Assert
+      const expected = [
+        `${parentFieldName}.0.${fieldName} must be a valid ISO 8601 date string`,
+        `${parentFieldName}.1.${fieldName} must be a valid ISO 8601 date string`,
+      ];
+
+      expect(body.message).toStrictEqual(expected);
+    });
+  });
+
+  describe(`when ${fieldName} is a boolean, true`, () => {
+    let mockPayload;
+
+    beforeAll(() => {
+      // Arrange
+      mockPayload = generatePayloadArrayOfObjects({ ...payloadParams, value: true });
+    });
+
+    it('should return a 400 response', async () => {
+      // Act
+      const response = await api.post(url, mockPayload);
+
+      // Assert
+      assert400Response(response);
+    });
+
+    it('should return the correct error messages', async () => {
+      // Act
+      const { body } = await api.post(url, mockPayload);
+
+      // Assert
+      const expected = [
+        `${parentFieldName}.0.${fieldName} must be a valid ISO 8601 date string`,
+        `${parentFieldName}.1.${fieldName} must be a valid ISO 8601 date string`,
+      ];
+
+      expect(body.message).toStrictEqual(expected);
+    });
+  });
+
+  describe(`when ${fieldName} is a boolean, false`, () => {
+    let mockPayload;
+
+    beforeAll(() => {
+      // Arrange
+      mockPayload = generatePayloadArrayOfObjects({ ...payloadParams, value: false });
+    });
+
+    it('should return a 400 response', async () => {
+      // Act
+      const response = await api.post(url, mockPayload);
+
+      // Assert
+      assert400Response(response);
+    });
+
+    it('should return the correct error messages', async () => {
+      // Act
+      const { body } = await api.post(url, mockPayload);
+
+      // Assert
+      const expected = [
+        `${parentFieldName}.0.${fieldName} must be a valid ISO 8601 date string`,
+        `${parentFieldName}.1.${fieldName} must be a valid ISO 8601 date string`,
+      ];
+
+      expect(body.message).toStrictEqual(expected);
+    });
+  });
+
+  describe(`when ${fieldName} is a number, 0`, () => {
+    let mockPayload;
+
+    beforeAll(() => {
+      // Arrange
+      mockPayload = generatePayloadArrayOfObjects({ ...payloadParams, value: 0 });
+    });
+
+    it('should return a 400 response', async () => {
+      // Act
+      const response = await api.post(url, mockPayload);
+
+      // Assert
+      assert400Response(response);
+    });
+
+    it('should return the correct error messages', async () => {
+      // Act
+      const { body } = await api.post(url, mockPayload);
+
+      // Assert
+      const expected = [
+        `${parentFieldName}.0.${fieldName} must be a valid ISO 8601 date string`,
+        `${parentFieldName}.1.${fieldName} must be a valid ISO 8601 date string`,
+      ];
+
+      expect(body.message).toStrictEqual(expected);
+    });
+  });
+
+  describe(`when ${fieldName} is a number, 1`, () => {
+    let mockPayload;
+
+    beforeAll(() => {
+      // Arrange
+      mockPayload = generatePayloadArrayOfObjects({ ...payloadParams, value: 1 });
+    });
+
+    it('should return a 400 response', async () => {
+      // Act
+      const response = await api.post(url, mockPayload);
+
+      // Assert
+      assert400Response(response);
+    });
+
+    it('should return the correct error messages', async () => {
+      // Act
+      const { body } = await api.post(url, mockPayload);
+
+      // Assert
+      const expected = [
+        `${parentFieldName}.0.${fieldName} must be a valid ISO 8601 date string`,
+        `${parentFieldName}.1.${fieldName} must be a valid ISO 8601 date string`,
+      ];
+
+      expect(body.message).toStrictEqual(expected);
+    });
+  });
+
+  describe(`when ${fieldName} is an empty string`, () => {
+    let mockPayload;
+
+    beforeAll(() => {
+      // Arrange
+      mockPayload = generatePayloadArrayOfObjects({ ...payloadParams, value: '' });
+    });
+
+    it('should return a 400 response', async () => {
+      // Act
+      const response = await api.post(url, mockPayload);
+
+      // Assert
+      assert400Response(response);
+    });
+
+    it('should return the correct error messages', async () => {
+      // Act
+      const { body } = await api.post(url, mockPayload);
+
+      // Assert
+      const expected = [
+        `${parentFieldName}.0.${fieldName} must be a valid ISO 8601 date string`,
+        `${parentFieldName}.1.${fieldName} must be a valid ISO 8601 date string`,
+      ];
+
+      expect(body.message).toStrictEqual(expected);
+    });
+  });
+
+  describe(`when ${fieldName} is below the minimum`, () => {
+    let mockPayload;
+
+    const value = 'a'.repeat(min - 1);
+
+    beforeAll(() => {
+      // Arrange
+      mockPayload = generatePayloadArrayOfObjects({ ...payloadParams, value });
+    });
+
+    it('should return a 400 response', async () => {
+      // Act
+      const response = await api.post(url, mockPayload);
+
+      // Assert
+      assert400Response(response);
+    });
+
+    it('should return the correct error messages', async () => {
+      // Act
+      const { body } = await api.post(url, mockPayload);
+
+      // Assert
+      const expected = [
+        `${parentFieldName}.0.${fieldName} must be a valid ISO 8601 date string`,
+        `${parentFieldName}.1.${fieldName} must be a valid ISO 8601 date string`,
+      ];
+
+      expect(body.message).toStrictEqual(expected);
+    });
+  });
+
+  describe(`when ${fieldName} is above the maximum`, () => {
+    let mockPayload;
+
+    const value = 'a'.repeat(max + 1);
+
+    beforeAll(() => {
+      // Arrange
+      mockPayload = generatePayloadArrayOfObjects({ ...payloadParams, value });
+    });
+
+    it('should return a 400 response', async () => {
+      // Act
+      const response = await api.post(url, mockPayload);
+
+      // Assert
+      assert400Response(response);
+    });
+
+    it('should return the correct error messages', async () => {
+      // Act
+      const { body } = await api.post(url, mockPayload);
+
+      // Assert
+      const expected = [
+        `${parentFieldName}.0.${fieldName} must be a valid ISO 8601 date string`,
+        `${parentFieldName}.1.${fieldName} must be a valid ISO 8601 date string`,
+      ];
+
+      expect(body.message).toStrictEqual(expected);
+    });
+  });
+
+  describe(`when ${fieldName} is NOT a valid ISO_639-1 date string`, () => {
+    let mockPayload;
+
+    const value = '01-02-2025';
+
+    beforeAll(() => {
+      // Arrange
+      mockPayload = generatePayloadArrayOfObjects({ ...payloadParams, value });
+    });
+
+    it('should return a 400 response', async () => {
+      // Act
+      const response = await api.post(url, mockPayload);
+
+      // Assert
+      assert400Response(response);
+    });
+
+    it('should return the correct error messages', async () => {
+      // Act
+      const { body } = await api.post(url, mockPayload);
+
+      // Assert
+      const expected = [
+        `${parentFieldName}.0.${fieldName} must be a valid ISO 8601 date string`,
+        `${parentFieldName}.1.${fieldName} must be a valid ISO 8601 date string`,
+      ];
+
+      expect(body.message).toStrictEqual(expected);
+    });
+  });
+};
