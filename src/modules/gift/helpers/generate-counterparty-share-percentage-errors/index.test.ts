a9b585aee0a605d3cd775fb136e57131371258c0
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2025-07-11 15:05:39 +0100

    fix(GIFT-13101): fix counterparty share percentage dto/validation

--- src/modules/gift/helpers/generate-counterparty-share-percentage-errors/index.test.ts
+++ src/modules/gift/helpers/generate-counterparty-share-percentage-errors/index.test.ts
@@ -56,6 +56,58 @@ describe('modules/gift/helpers/generate-counterparty-share-percentage-errors', (
     });
   });
 
+  describe(`when a provided role requires a sharePercentage, is provided and below ${MIN}`, () => {
+    it('should return an array with validation errors', () => {
+      // Arrange
+      const mockProvidedRoles = [{ ...EXAMPLES.GIFT.COUNTERPARTY(), roleCode: '2', sharePercentage: MIN - 1 }];
+
+      // Act
+      const result = generateCounterpartySharePercentageErrors({
+        counterpartyRoles: mockCounterpartyRoles,
+        providedCounterparties: mockProvidedRoles,
+      });
+
+      // Assert
+      const expected = [`counterparties.0.sharePercentage must be a provided as a number, at least ${MIN} and not greater than ${MAX}`];
+
+      expect(result).toStrictEqual(expected);
+    });
+  });
+
+  describe(`when a provided role requires a sharePercentage, is provided and above ${MAX}`, () => {
+    it('should return an array with validation errors', () => {
+      // Arrange
+      const mockProvidedRoles = [{ ...EXAMPLES.GIFT.COUNTERPARTY(), roleCode: '2', sharePercentage: MAX + 1 }];
+
+      // Act
+      const result = generateCounterpartySharePercentageErrors({
+        counterpartyRoles: mockCounterpartyRoles,
+        providedCounterparties: mockProvidedRoles,
+      });
+
+      // Assert
+      const expected = [`counterparties.0.sharePercentage must be a provided as a number, at least ${MIN} and not greater than ${MAX}`];
+
+      expect(result).toStrictEqual(expected);
+    });
+  });
+
+  describe(`when a provided role requires a sharePercentage, is provided and between ${MIN} and ${MAX}`, () => {
+    it('should return an empty array', () => {
+      // Arrange
+      const mockProvidedRoles = [{ ...EXAMPLES.GIFT.COUNTERPARTY(), roleCode: '2', sharePercentage: MAX - 1 }];
+
+      // Act
+      const result = generateCounterpartySharePercentageErrors({
+        counterpartyRoles: mockCounterpartyRoles,
+        providedCounterparties: mockProvidedRoles,
+      });
+
+      // Assert
+      expect(result).toStrictEqual([]);
+    });
+  });
+
   describe('when one provided role requires a sharePercentage, but is not provided', () => {
     it('should return an array with validation errors', () => {
       // Arrange
