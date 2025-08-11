37068a44c0297b404c03aadcbca8eeb2d9fb9503
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2025-08-11 14:01:44 +0100

    feat(GIFT-13955): minor code improvement

--- src/modules/gift/services/gift.business-calendar.service.ts
+++ src/modules/gift/services/gift.business-calendar.service.ts
@@ -38,10 +38,12 @@ export class GiftBusinessCalendarService {
     try {
       this.logger.info('Creating a business calendar for facility %s', facilityId);
 
+      const centreCode = BUSINESS_CALENDAR.LONDON.CODE;
+
       const response = await this.giftHttpService.post<GiftBusinessCalendarResponseDto>({
         path: `${PATH.FACILITY}/${facilityId}${PATH.WORK_PACKAGE}/${workPackageId}${PATH.CONFIGURATION_EVENT}/${EVENT_TYPES.ADD_BUSINESS_CALENDAR}`,
         payload: {
-          centreCode: BUSINESS_CALENDAR.LONDON.CODE,
+          centreCode,
           startDate,
           exitDate,
         },
