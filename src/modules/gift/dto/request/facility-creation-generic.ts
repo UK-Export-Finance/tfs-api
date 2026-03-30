8c82297f2e19542655afb7bbf3996a20867b2634
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2026-03-30 17:28:42 +0100

    feat(GIFT-22093): create a GIFT facility - allow empty fixedFees array

--- src/modules/gift/dto/request/facility-creation-generic.ts
+++ src/modules/gift/dto/request/facility-creation-generic.ts
@@ -59,7 +59,6 @@ export class GiftFacilityCreationGenericRequestDto {
     type: GiftFixedFeeRequestDto,
   })
   @IsArray()
-  @ArrayNotEmpty()
   @IsDefined()
   @Type(() => GiftFixedFeeRequestDto)
   @ValidateNested()
