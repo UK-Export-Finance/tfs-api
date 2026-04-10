87365495e38b342638e02e609a6b0ed4d45eda1d
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2026-04-10 14:21:07 +0100

    feat(GIFT-22217): dto validation

--- src/modules/gift/dto/request/accrual-schedule.ts
+++ src/modules/gift/dto/request/accrual-schedule.ts
@@ -1,37 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { EXAMPLES } from '@ukef/constants';
-import { IsDateString, IsDefined, IsNumber, IsString } from 'class-validator';
+import { EXAMPLES, GIFT } from '@ukef/constants';
+import { IsDateString, IsDefined, IsNumber, IsString, Length, Min } from 'class-validator';
 
 const {
   GIFT: { ACCRUAL_SCHEDULE: EXAMPLE },
 } = EXAMPLES;
 
-// const {
-//   VALIDATION: { ACCRUAL_SCHEDULE: VALIDATION },
-// } = GIFT;
+const {
+  VALIDATION: { ACCRUAL_SCHEDULE: VALIDATION },
+} = GIFT;
 
 /**
  * GIFT "accrual schedule" request DTO.
  * These fields are required for APIM to create an "accrual schedule" in GIFT.
  */
 export class GiftAccrualScheduleRequestDto {
-  // @IsDefined()
-  // @IsNumber()
-  // @ApiProperty({
-  //   example: EXAMPLE.obligationId,
-  //   description: 'The ID of the obligation',
-  //   required: true,
-  // })
-  // obligationId: number;
-
   @IsDefined()
   @IsString()
+  @Length(VALIDATION.ACCURAL_DAY_BASIS_CODE.MIN_LENGTH, VALIDATION.ACCURAL_DAY_BASIS_CODE.MAX_LENGTH)
   @ApiProperty({
-    example: EXAMPLE.accrualScheduleTypeCode,
-    description: 'The accrual schedule type code',
+    example: EXAMPLE.accrualDayBasisCode,
+    description: 'The accrual day basis code',
     required: true,
   })
-  accrualScheduleTypeCode: string;
+  accrualDayBasisCode: string;
 
   @IsDefined()
   @IsDateString()
@@ -41,6 +33,16 @@ export class GiftAccrualScheduleRequestDto {
     required: true,
   })
   accrualEffectiveDate: string;
+
+  @IsDefined()
+  @IsString()
+  @Length(VALIDATION.ACCRUAL_FREQUENCY_CODE.MIN_LENGTH, VALIDATION.ACCRUAL_FREQUENCY_CODE.MAX_LENGTH)
+  @ApiProperty({
+    example: EXAMPLE.accrualFrequencyCode,
+    description: 'The accrual frequency code',
+    required: true,
+  })
+  accrualFrequencyCode: string;
 
   @IsDefined()
   @IsDateString()
@@ -53,30 +55,23 @@ export class GiftAccrualScheduleRequestDto {
 
   @IsDefined()
   @IsString()
+  @Length(VALIDATION.ACCRUAL_SCHEDULE_TYPE_CODE.MIN_LENGTH, VALIDATION.ACCRUAL_SCHEDULE_TYPE_CODE.MAX_LENGTH)
   @ApiProperty({
-    example: EXAMPLE.accrualFrequencyCode,
-    description: 'The accrual frequency code',
+    example: EXAMPLE.accrualScheduleTypeCode,
+    description: 'The accrual schedule type code',
     required: true,
   })
-  accrualFrequencyCode: string;
+  accrualScheduleTypeCode: string;
 
   @IsDefined()
-  @IsDateString()
+  @IsNumber()
+  @Min(VALIDATION.ADDITIONAL_RATE.MIN)
   @ApiProperty({
-    example: EXAMPLE.firstCycleAccrualEndDate,
-    description: 'The first cycle accrual end date',
+    example: EXAMPLE.additionalRate,
+    description: 'The additional rate',
     required: true,
   })
-  firstCycleAccrualEndDate: string;
-
-  @IsDefined()
-  @IsString()
-  @ApiProperty({
-    example: EXAMPLE.accrualDayBasisCode,
-    description: 'The accrual day basis code',
-    required: true,
-  })
-  accrualDayBasisCode: string;
+  additionalRate: number;
 
   @IsDefined()
   @IsNumber()
@@ -88,20 +83,21 @@ export class GiftAccrualScheduleRequestDto {
   baseRate: number;
 
   @IsDefined()
+  @IsDateString()
+  @ApiProperty({
+    example: EXAMPLE.firstCycleAccrualEndDate,
+    description: 'The first cycle accrual end date',
+    required: true,
+  })
+  firstCycleAccrualEndDate: string;
+
+  @IsDefined()
   @IsNumber()
+  @Min(VALIDATION.SPREAD_RATE.MIN)
   @ApiProperty({
     example: EXAMPLE.spreadRate,
     description: 'The spread rate',
     required: true,
   })
   spreadRate: number;
-
-  @IsDefined()
-  @IsNumber()
-  @ApiProperty({
-    example: EXAMPLE.additionalRate,
-    description: 'The additional rate',
-    required: true,
-  })
-  additionalRate: number;
 }
