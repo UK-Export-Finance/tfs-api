1746e31ec20e6552d0f8288fd1d4a208bef12927
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2026-04-30 11:52:37 +0100

    feat(GIFT-23456): create a gift facility - default linkedRepaymentProfileId (#1330)

--- src/modules/gift/dto/request/obligation.ts
+++ src/modules/gift/dto/request/obligation.ts
@@ -3,7 +3,7 @@ import { EXAMPLES, GIFT } from '@ukef/constants';
 import { IsDateString, IsDefined, IsNumber, IsOptional, IsString, Length, Max, Min } from 'class-validator';
 
 const {
-  GIFT: { OBLIGATION },
+  GIFT: { OBLIGATION, REPAYMENT_PROFILE_ID, REPAYMENT_TYPE },
 } = EXAMPLES;
 
 const {
@@ -11,12 +11,13 @@ const {
 } = GIFT;
 
 const EXAMPLE = OBLIGATION() as {
+  amount: number;
   currency: string;
   effectiveDate: string;
+  linkedRepaymentProfileId: number;
   maturityDate: string;
-  amount: number;
-  subtypeCode: string;
   repaymentType: string;
+  subtypeCode: string;
 };
 
 /**
@@ -53,6 +54,17 @@ export class GiftObligationRequestDto {
     required: true,
   })
   effectiveDate: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(VALIDATION.LINKED_REPAYMENT_PROFILE_ID.MIN)
+  @Max(VALIDATION.LINKED_REPAYMENT_PROFILE_ID.MAX)
+  @ApiProperty({
+    example: REPAYMENT_PROFILE_ID,
+    description: `Optional linked repayment profile ID. overview.repaymentType is "${REPAYMENT_TYPE.BULLET}" (and therefore repayment profiles are required) Note that this currently always defaults to null`,
+    required: false,
+  })
+  linkedRepaymentProfileId?: number;
 
   @IsDefined()
   @IsDateString()
