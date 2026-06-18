031eed065a99e9a72494741176a32b9ec4e1eb22
Tony Barnes <Tony.Barnes@ukexportfinance.gov.uk>
2026-06-18 09:05:07 +0100

    chore(DTFS2-8487): create a gift facility - optional calendar dates (#1399)

--- src/modules/gift/services/gift.business-calendar.service.test.ts
+++ src/modules/gift/services/gift.business-calendar.service.test.ts
@@ -8,7 +8,7 @@ const {
   GIFT: { BUSINESS_CALENDAR, FACILITY_ID: mockFacilityId, WORK_PACKAGE_ID: mockWorkPackageId },
 } = EXAMPLES;
 
-const { EVENT_TYPES, PATH } = GIFT;
+const { EVENT_TYPES, INTEGRATION_DEFAULTS, PATH } = GIFT;
 
 describe('GiftBusinessCalendarService', () => {
   const logger = new PinoLogger({});
@@ -40,6 +40,68 @@ describe('GiftBusinessCalendarService', () => {
   });
 
   describe('createOne', () => {
+    describe('when optional date fields are provided', () => {
+      it('should call giftHttpService.post with the provided date fields', async () => {
+        // Arrange
+        const payload = {
+          ...BUSINESS_CALENDAR,
+          startDate: '2024-01-01',
+          exitDate: '2024-02-01',
+        };
+
+        // Act
+        await service.createOne({
+          facilityId: mockFacilityId,
+          workPackageId: mockWorkPackageId,
+          startDate: payload.startDate,
+          exitDate: payload.exitDate,
+        });
+
+        // Assert
+        expect(mockHttpServicePost).toHaveBeenCalledTimes(1);
+
+        const expected = {
+          path: `${PATH.FACILITY}/${mockFacilityId}${PATH.WORK_PACKAGE}/${mockWorkPackageId}${PATH.CONFIGURATION_EVENT}/${EVENT_TYPES.ADD_BUSINESS_CALENDAR}`,
+          payload,
+        };
+
+        expect(mockHttpServicePost).toHaveBeenCalledWith(expected);
+      });
+    });
+
+    describe('when optional date fields are NOT provided', () => {
+      it('should call giftHttpService.post with default date field values', async () => {
+        // Arrange
+        const payload = {
+          ...BUSINESS_CALENDAR,
+          startDate: undefined,
+          exitDate: undefined,
+        };
+
+        // Act
+        await service.createOne({
+          facilityId: mockFacilityId,
+          workPackageId: mockWorkPackageId,
+          startDate: payload.startDate,
+          exitDate: payload.exitDate,
+        });
+
+        // Assert
+        expect(mockHttpServicePost).toHaveBeenCalledTimes(1);
+
+        const expected = {
+          path: `${PATH.FACILITY}/${mockFacilityId}${PATH.WORK_PACKAGE}/${mockWorkPackageId}${PATH.CONFIGURATION_EVENT}/${EVENT_TYPES.ADD_BUSINESS_CALENDAR}`,
+          payload: {
+            ...payload,
+            exitDate: INTEGRATION_DEFAULTS.BUSINESS_CALENDAR_EXIT_DATE,
+            startDate: INTEGRATION_DEFAULTS.BUSINESS_CALENDAR_START_DATE,
+          },
+        };
+
+        expect(mockHttpServicePost).toHaveBeenCalledWith(expected);
+      });
+    });
+
     it('should call giftHttpService.post', async () => {
       // Act
       await service.createOne({
